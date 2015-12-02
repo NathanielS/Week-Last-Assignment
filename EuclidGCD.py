b9dd24c655dae1d95c94f3f1413e08ac627f9f29
nathanielsmith <nathaniel.smith@myemail.indwes.edu>
2015-12-02 19:29:47 +0000

    done

--- EuclidGCD.py
+++ EuclidGCD.py
@@ -1,4 +1,14 @@
 def gCd(x, y):
-    if ???# base case here
-        return ???
-    return gCd(?????)
+    print("x= ",x,"y= ",y)
+    if y == 0:
+        return x
+    else:
+        return gCd(y,x%y)
+
+def main():
+    x = 252
+    y = 105
+    print("GCD of",x,",",y,"=",gCd(x,y))
+    
+if __name__ == "__main__":
+    main()
